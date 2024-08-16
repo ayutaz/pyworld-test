a41841c39c403d4f496db73cae84ec607c3d22d0
yousan <ka1357amnbpdr@gmail.com>
2024-08-17 02:38:12 +0900

    fix: clothフェードを使ったモーフィングを試した

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ import pyworld as pw
 import soundfile as sf
 from soxr import resample
 
+
+def align_waves(wav1, wav2):
+    # 長い方の波形を短い方に合わせる
+    if len(wav1) > len(wav2):
+        wav1 = wav1[:len(wav2)]
+    else:
+        wav2 = wav2[:len(wav1)]
+    return wav1, wav2
+
+
 def synthesis_morphing_parameter(wav1, wav2, fs):
     frame_period = 5.0
+
+    # 波形の長さを合わせる
+    wav1, wav2 = align_waves(wav1, wav2)
+
     f0_1, time_axis_1 = pw.harvest(wav1, fs, frame_period=frame_period)
     sp_1 = pw.cheaptrick(wav1, f0_1, time_axis_1, fs)
     ap_1 = pw.d4c(wav1, f0_1, time_axis_1, fs)
@@ -13,14 +27,10 @@ def synthesis_morphing_parameter(wav1, wav2, fs):
     sp_2 = pw.cheaptrick(wav2, f0_2, time_axis_2, fs)
     ap_2 = pw.d4c(wav2, f0_2, time_axis_2, fs)
 
-    # Adjust the length of parameters
-    min_len = min(len(f0_1), len(f0_2))
-    f0_1, sp_1, ap_1 = f0_1[:min_len], sp_1[:min_len], ap_1[:min_len]
-    f0_2, sp_2, ap_2 = f0_2[:min_len], sp_2[:min_len], ap_2[:min_len]
-
     return f0_1, sp_1, ap_1, f0_2, sp_2, ap_2, fs, frame_period
 
-def synthesize_morphed_wave(f0_1, sp_1, ap_1, f0_2, sp_2, ap_2, fs, frame_period, morph_rate, output_fs):
+
+def synthesize_morphed_wave(f0_1, sp_1, ap_1, f0_2, sp_2, ap_2, fs, frame_period, morph_rate):
     if morph_rate < 0.0 or morph_rate > 1.0:
         raise ValueError("morph_rateは0.0から1.0の範囲で指定してください")
 
@@ -30,10 +40,8 @@ def synthesize_morphed_wave(f0_1, sp_1, ap_1, f0_2, sp_2, ap_2, fs, frame_period
 
     y_h = pw.synthesize(f0_morph, sp_morph, ap_morph, fs, frame_period)
 
-    if output_fs != fs:
-        y_h = resample(y_h, fs, output_fs)
-
     return y_h.astype(np.float32)
+
 
 # メイン処理
 wav1, fs = sf.read('voice1.wav')
@@ -46,8 +54,12 @@ f0_1, sp_1, ap_1, f0_2, sp_2, ap_2, fs, frame_period = synthesis_morphing_parame
 morph_rate = 0.5
 
 # モーフィングした音声を合成
-output_fs = fs  # 出力のサンプリングレートを指定（ここではfsと同じに設定）
-morphed_wave = synthesize_morphed_wave(f0_1, sp_1, ap_1, f0_2, sp_2, ap_2, fs, frame_period, morph_rate, output_fs)
+morphed_wave = synthesize_morphed_wave(f0_1, sp_1, ap_1, f0_2, sp_2, ap_2, fs, frame_period, morph_rate)
 
 # 合成した音声を保存
-sf.write('morphed_voice1.wav', morphed_wave, output_fs)
+sf.write('morphed_voice.wav', morphed_wave, fs)
+
+# オリジナルの音声も保存（比較用）
+wav1, wav2 = align_waves(wav1, wav2)
+sf.write('original_voice1.wav', wav1, fs)
+sf.write('original_voice2.wav', wav2, fs)
