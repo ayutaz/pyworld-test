6f67f7f442d9d2ddab55307ea4be99e87843ce12
yousan <ka1357amnbpdr@gmail.com>
2024-08-17 12:37:33 +0900

    fix: DTWを使ったボイスモーフィングの実装

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import numpy as np
 import pyworld as pw
 import soundfile as sf
-from soxr import resample
+from dtaidistance import dtw
 
 
-def align_waves(wav1, wav2):
-    # 長い方の波形を短い方に合わせる
-    if len(wav1) > len(wav2):
-        wav1 = wav1[:len(wav2)]
-    else:
-        wav2 = wav2[:len(wav1)]
-    return wav1, wav2
+def align_with_dtw(wav1, wav2):
+    # DTWで最適なパスを見つける
+    path = dtw.warping_path(wav1, wav2)
+
+
+    # パスに基づいて wav2 を伸縮する
+    wav2_warped = np.zeros_like(wav1)
+    for i, j in path:
+        wav2_warped[i] = wav2[j]
+
+    return wav1, wav2_warped
 
 
 def synthesis_morphing_parameter(wav1, wav2, fs):
     frame_period = 5.0
 
-    # 波形の長さを合わせる
-    wav1, wav2 = align_waves(wav1, wav2)
+    # DTWで波形を揃える
+    wav1, wav2 = align_with_dtw(wav1, wav2)
 
     f0_1, time_axis_1 = pw.harvest(wav1, fs, frame_period=frame_period)
     sp_1 = pw.cheaptrick(wav1, f0_1, time_axis_1, fs)
@@ -26,6 +30,11 @@ def synthesis_morphing_parameter(wav1, wav2, fs):
     f0_2, time_axis_2 = pw.harvest(wav2, fs, frame_period=frame_period)
     sp_2 = pw.cheaptrick(wav2, f0_2, time_axis_2, fs)
     ap_2 = pw.d4c(wav2, f0_2, time_axis_2, fs)
+
+    # パラメータの長さを揃える
+    min_len = min(len(f0_1), len(f0_2))
+    f0_1, sp_1, ap_1 = f0_1[:min_len], sp_1[:min_len], ap_1[:min_len]
+    f0_2, sp_2, ap_2 = f0_2[:min_len], sp_2[:min_len], ap_2[:min_len]
 
     return f0_1, sp_1, ap_1, f0_2, sp_2, ap_2, fs, frame_period
 
@@ -60,6 +69,6 @@ morphed_wave = synthesize_morphed_wave(f0_1, sp_1, ap_1, f0_2, sp_2, ap_2, fs, f
 sf.write('morphed_voice.wav', morphed_wave, fs)
 
 # オリジナルの音声も保存（比較用）
-wav1, wav2 = align_waves(wav1, wav2)
+wav1, wav2 = align_with_dtw(wav1, wav2)
 sf.write('original_voice1.wav', wav1, fs)
 sf.write('original_voice2.wav', wav2, fs)
